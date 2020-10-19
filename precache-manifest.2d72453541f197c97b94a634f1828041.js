4241ac5e8a355eb855701fd88f1ca3b33c9dc126
abidasobhan19 <abidasobhan19@gmail.com>
2020-10-19 06:51:08 -0700

    Updates

---
+++ precache-manifest.2d72453541f197c97b94a634f1828041.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6f164d9614a16877e2c27cbef25d018d",
+    "url": "/Construction_Manager/index.html"
+  },
+  {
+    "revision": "b7971c64aba6a231f48f",
+    "url": "/Construction_Manager/static/css/main.d805107c.chunk.css"
+  },
+  {
+    "revision": "467b43480b18e4682c35",
+    "url": "/Construction_Manager/static/js/2.f934cbe8.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/Construction_Manager/static/js/2.f934cbe8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b7971c64aba6a231f48f",
+    "url": "/Construction_Manager/static/js/main.a78d073a.chunk.js"
+  },
+  {
+    "revision": "edd150a4491e63e3f195",
+    "url": "/Construction_Manager/static/js/runtime-main.15d518e5.js"
+  }
+]);
