ffdf73cf7370cea36303ceb582d48ccb0098132a
abidasobhan19 <abidasobhan19@gmail.com>
2020-10-19 06:40:38 -0700

    initial commit

---
+++ src/Component/main.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Site from "./Construction_manager/index";
+
+const Main = () => {
+  return (
+    <div>
+      <Site />
+    </div>
+  );
+};
+
+export default Main;
